c70415076e19791ebb64e3f1560277d9d0480f89
DuretiShemsi <98233210+DuretiShemsi@users.noreply.github.com>
2023-01-07 04:54:03 -0800

    Exercises

---
+++ src/main/java/com/shemsi/loops/Square.java
@@ -0,0 +1,13 @@
+package com.shemsi.loops;
+
+public class Square {
+    public static void main(String[] args) {
+        for (int x = 1; x <= 10; x++) {
+            for (int y = 1; y <= 20; y++) {
+                System.out.print("x");
+            }
+            System.out.println();
+
+        }
+    }
+}
